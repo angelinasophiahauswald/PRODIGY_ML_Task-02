a73faae8ba5d313c689a6f75bf36633555f11dc6
Angelina <ashauswald@gmail.com>
2024-02-22 21:54:43 +0100

    rework assign_clusters

--- src/k_means.py
+++ src/k_means.py
@@ -13,39 +13,44 @@ def preprocessing_non_numericals(df):
     df['Gender'] = df['Gender'].replace({'Female': 1, 'Male': 0})
     return df
 
-# Function to calculate distance between two data points
+# Function to calculate euclidean distance between two data points
 def calculate_distance(point1, point2):
     return np.sqrt(np.sum((point1 - point2)**2))
 
-# Function to assign points to clusters
+# Function to assign points to nearest cluster
 def assign_clusters(X, centroids):
     distances = np.zeros((X.shape[0], centroids.shape[0]))
-    for i, centroid in enumerate(centroids):
-        for j, point in enumerate(X):
-            distances[j, i] = calculate_distance(centroid, point)
+    x_index = 0
+    for data_point in X:
+        centroid_index = 0
+        for centroid in centroids:
+            distance = calculate_distance(centroid, data_point)
+            distances[x_index, centroid_index] = distance
+            centroid_index = centroid_index + 1
+        x_index = x_index + 1
     return np.argmin(distances, axis=1)
 
 # Function to update centroids
-def update_centroids(X, labels, num_clusters):
-    centroids = np.zeros((num_clusters, X.shape[1]))
-    for i in range(num_clusters):
-        centroids[i] = np.mean(X[labels == i], axis=0)
+def update_centroids(X, cluster, k):
+    centroids = np.zeros((k, X.shape[1]))
+    for i in range(k):
+        centroids[i] = np.mean(X[cluster == i], axis=0)
     return centroids
 
 # Function to perform K-means clustering
-def kmeans(X, num_clusters, max_iter=100):
+def k_means(X, k, iteration):
     # Initialize centroids randomly
-    centroids = X[np.random.choice(X.shape[0], num_clusters, replace=False)]
-    for _ in range(max_iter):
+    centroids = X[np.random.choice(X.shape[0], k, replace=False)]
+    for i in range(iteration):
         # Assign points to the closest cluster centroid
-        labels = assign_clusters(X, centroids)
+        cluster = assign_clusters(X, centroids)
         # Update centroids
-        new_centroids = update_centroids(X, labels, num_clusters)
+        new_centroids = update_centroids(X, cluster, k)
         # Check for convergence
         if np.allclose(centroids, new_centroids):
             break
         centroids = new_centroids
-    return labels, centroids
+    return cluster, centroids
 
 # Function to visualize clusters
 def plot_clusters(X, centroids, labels, save_path=None):
@@ -71,8 +76,6 @@ if __name__ == '__main__':
     k = sys.argv[2]
     k = int(k)
     x = read_data(data)
-    print(preprocessing_non_numericals(x))
-
-    labels, centroids = kmeans(x.values, k)
-    print(labels)
-    plot_clusters(x.values, centroids, labels, save_path='clusters_plot.png')
+    preprocessing_non_numericals(x)
+    cluster, centroids = k_means(x.values, k, 100)
+    plot_clusters(x.values, centroids, cluster, save_path='clusters_plot.png')
